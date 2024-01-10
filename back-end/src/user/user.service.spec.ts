7b1a9936e29d81da0f2316188d5d8a6e46357111
Arnaud Rinquin <rinquin.arnaud@gmail.com>
2024-01-10 20:19:17 +0100

    chore(back): Fix jest absolute module resolution and have user.service specs pass

--- back-end/src/user/user.service.spec.ts
+++ back-end/src/user/user.service.spec.ts
@@ -1,12 +1,50 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { UserService } from './user.service';
+import { MongooseModule } from '@nestjs/mongoose';
+import { User, UserSchema } from './schema/user.schema';
+import { UserMapper } from './mapper/user.mapper';
+import { ConfigModule } from '@nestjs/config';
+import { GraphQLModule } from '@nestjs/graphql';
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
+import { AuthModule } from 'src/auth/auth.module';
+import { UserModule } from './user.module';
+import { MeModule } from 'src/me/me.module';
+import { ActivityModule } from 'src/activity/activity.module';
+import { SeedModule } from 'src/seed/seed.module';
+import { AppController } from 'src/app.controller';
+import { AppService } from 'src/app.service';
+import { SeedService } from 'src/seed/seed.service';
 
 describe('UserService', () => {
   let service: UserService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [UserService],
+      imports: [
+        ConfigModule.forRoot({ isGlobal: true }),
+        GraphQLModule.forRoot<ApolloDriverConfig>({
+          driver: ApolloDriver,
+          autoSchemaFile: 'schema.gql',
+          sortSchema: true,
+          buildSchemaOptions: { numberScalarMode: 'integer' },
+          context: ({ req, res }: { req: Request; res: Response }) => ({
+            req,
+            res,
+          }),
+        }),
+        MongooseModule.forRootAsync({
+          useFactory: () => {
+            return { uri: process.env.MONGO_URI };
+          },
+        }),
+        AuthModule,
+        UserModule,
+        MeModule,
+        ActivityModule,
+        SeedModule,
+      ],
+      controllers: [AppController],
+      providers: [AppService, SeedService],
     }).compile();
 
     service = module.get<UserService>(UserService);
