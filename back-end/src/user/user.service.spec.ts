22c647e6fbe56c07504af9040ab1a997bd32679c
Arnaud Rinquin <rinquin.arnaud@gmail.com>
2024-01-16 20:02:20 +0100

    chore(back): Clean-up user-module tests

--- back-end/src/user/user.service.spec.ts
+++ back-end/src/user/user.service.spec.ts
@@ -2,16 +2,7 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { UserService } from './user.service';
 import { MongooseModule } from '@nestjs/mongoose';
 import { ConfigModule } from '@nestjs/config';
-import { GraphQLModule } from '@nestjs/graphql';
-import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
-import { AuthModule } from 'src/auth/auth.module';
 import { UserModule } from './user.module';
-import { MeModule } from 'src/me/me.module';
-import { ActivityModule } from 'src/activity/activity.module';
-import { SeedModule } from 'src/seed/seed.module';
-import { AppController } from 'src/app.controller';
-import { AppService } from 'src/app.service';
-import { SeedService } from 'src/seed/seed.service';
 import { randomUUID } from 'crypto';
 
 describe('UserService', () => {
@@ -21,29 +12,13 @@ describe('UserService', () => {
     const module: TestingModule = await Test.createTestingModule({
       imports: [
         ConfigModule.forRoot({ isGlobal: true }),
-        GraphQLModule.forRoot<ApolloDriverConfig>({
-          driver: ApolloDriver,
-          autoSchemaFile: 'schema.gql',
-          sortSchema: true,
-          buildSchemaOptions: { numberScalarMode: 'integer' },
-          context: ({ req, res }: { req: Request; res: Response }) => ({
-            req,
-            res,
-          }),
-        }),
         MongooseModule.forRootAsync({
           useFactory: () => {
             return { uri: process.env.MONGO_URI };
           },
         }),
-        AuthModule,
         UserModule,
-        MeModule,
-        ActivityModule,
-        SeedModule,
       ],
-      controllers: [AppController],
-      providers: [AppService, SeedService],
     }).compile();
 
     service = module.get<UserService>(UserService);
