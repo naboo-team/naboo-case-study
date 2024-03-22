4f60f36b87753779d8011981e91debb7a992ab05
Satcheen Shakya <satcheen@rubiku.be>
2024-03-22 11:45:51 +0100

    feat: update me resolver to use favourites functionality

--- back-end/src/me/resolver/me.resolver.ts
+++ back-end/src/me/resolver/me.resolver.ts
@@ -1,4 +1,4 @@
-import { Context, Query, Resolver } from '@nestjs/graphql';
+import { Args, Context, Mutation, Query, Resolver } from '@nestjs/graphql';
 import { UserMapper } from '../../user/mapper/user.mapper';
 import { UserService } from '../../user/user.service';
 import { UserDto } from '../../user/types/user.dto';
@@ -16,6 +16,34 @@ export class MeResolver {
   @UseGuards(AuthGuard)
   async getMe(@Context() context: any): Promise<UserDto> {
     const user = await this.userService.getById(context.user!.id);
+    return this.userMapper.convert(user);
+  }
+
+  @Mutation(() => UserDto)
+  @UseGuards(AuthGuard)
+  async addFavorite(
+    @Context() context: any,
+    @Args('activityId') activityId: string,
+  ): Promise<UserDto> {
+    const user = await this.userService.addFavorite(
+      context.user?.id,
+      activityId,
+    );
+    console.log('add', context.user);
+    return this.userMapper.convert(user);
+  }
+
+  @Mutation(() => UserDto)
+  @UseGuards(AuthGuard)
+  async removeFavorite(
+    @Context() context: any,
+    @Args('activityId') activityId: string,
+  ): Promise<UserDto> {
+    const user = await this.userService.removeFavorite(
+      context.user?.id,
+      activityId,
+    );
+    console.log('remove', context.user);
     return this.userMapper.convert(user);
   }
 }
