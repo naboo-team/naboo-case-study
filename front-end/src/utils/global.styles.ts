97be8d09c9fac257553cfd46d04b218247310286
Satcheen Shakya <satcheen@rubiku.be>
2024-03-22 12:03:12 +0100

    feat: add style to favourites

--- front-end/src/utils/global.styles.ts
+++ front-end/src/utils/global.styles.ts
@@ -10,4 +10,14 @@ export const useGlobalStyles = createStyles(() => ({
     textOverflow: "ellipsis",
     whiteSpace: "nowrap",
   },
+
+  heartIcon: {
+    position: "absolute",
+    top: "10px",
+    right: "10px",
+    zIndex: 2,
+    cursor: "pointer",
+    stroke: "none"
+  },
+
 }));
